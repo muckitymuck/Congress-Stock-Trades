ec7800d585722e95d33afd4c3bd96482166fd64e
muckity <andrewgoldstein@hotmail.com>
2022-07-05 07:59:54 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,37 +3,6 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var json = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
-
-fetch(json)
-    .then(res => res.json())
-    .then(data => {
-
-        console.log(data)
-        const districtsArray = [];
-        let uniqueDistrictsArray = [];
-        //gets all districts and console logs them
-        for (const [key, value] of Object.entries(data)){
-            //console.log(`${value.district}`)
-            districtsArray.push(value.district)
-            uniqueDistrictsArray = [...new Set(districtsArray)]
-            
-        }
-        uniqueDistrictsArray.sort();
-        console.log(uniqueDistrictsArray)
-        //populates the Select District Dropdown
-        var districtDropdown = document.getElementById("selectDistrict");
-
-       for(var i = 0; i < uniqueDistrictsArray.length; i++){
-            var opt = uniqueDistrictsArray[i];
-            var el = document.createElement("option");
-            el.textContent = opt;
-            el.value = opt;
-            selectDistrict.appendChild(el)
-            
-       }
-
-    })
 
 (function($) {
 
@@ -60,11 +29,46 @@ fetch(json)
 
 })(jQuery);
 
+//Load District Dropdown with valid Districts in the API
+var json = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
+
+fetch(json)
+    .then(res => res.json())
+    .then(data => {
+
+        //console.log(data)
+        const districtsArray = [];
+        let uniqueDistrictsArray = [];
+        //gets all districts and console logs them
+        for (const [key, value] of Object.entries(data)){
+            //console.log(`${value.district}`)
+            districtsArray.push(value.district)
+            uniqueDistrictsArray = [...new Set(districtsArray)]
+            
+        }
+        uniqueDistrictsArray.sort();
+        //populates the Select District Dropdown
+        var districtDropdown = document.getElementById("selectDistrictDD");
+
+       for(var i = 0; i < uniqueDistrictsArray.length; i++){
+            var opt = uniqueDistrictsArray[i];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            selectDistrictDD.appendChild(el)
+            
+       }
+       //log the Dropdown options from API
+       console.log(selectDistrictDD)
+
+    })
+
+
 document.querySelector('tableButton').addEventListener('click',getData)
+document.querySelector('districtDDBtn').addEventListener('click',getDistrictData)
 function getData() {
     let input = document.querySelector("input").value
-    let select = document.querySelector("select").value
-    let selectDistrict = document.querySelector("selectDistrict").value
+    let select = document.querySelector(".selectState").value
     fetch(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
@@ -83,12 +87,10 @@ function getData() {
             console.log(uniqueDistrictsArray)
 
             let filtered = ''
-            if (input.length <= 3 && select === "" && selectDistrict == ""){
+            if (input.length <= 3 && select === ""){
                 document.getElementById('data').innerHTML = "No Transactions.  Please Enter a District or State."
-               //if textbox is incomplete, filtered = State Dropdown 
-            }  if (selectDistrict != ""){
-                filtered = data.filter(a => a.disctrict == selectDistrict)
-            }
+
+             }  
                 if (input.length <= 3) {
                 filtered = data.filter(a => a.district.slice(0,2) == select)  
             } else{
@@ -110,7 +112,62 @@ function getData() {
                     temp += "<td>" + itemData.amount + "</td>";
                     temp += "<td>" + itemData.asset_description + "</td></tr>";
                 });
-                document.getElementById('data').innerHTML = temp;
+                document.getElementById('#data').innerHTML = temp;
+
+
+                
+            }
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+    });
+}
+function getDistrictData() {
+    let selectDistrict = document.querySelector("#selectDistrictDD").value
+    console.log(selectDistrict)
+    fetch(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json`)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+
+            const districtsArray = [];
+            let uniqueDistrictsArray = [];
+            //gets all districts and console logs them
+            for (const [key, value] of Object.entries(data)){
+                //console.log(`${value.district}`)
+                districtsArray.push(value.district)
+                uniqueDistrictsArray = [...new Set(districtsArray)]
+                
+            }
+            uniqueDistrictsArray.sort();
+
+            let filtered = ''
+            if (selectDistrictDD === ""){
+                document.getElementById('#data').innerHTML = "No Transactions.  Please Enter a District or State."
+                //if textbox is incomplete, filtered = State Dropdown 
+                }  //if (selectDistrict != ""){
+            //     filtered = data.filter(a => a.disctrict == selectDistrict.value)
+            //     console.log(selectDistrict)
+            // }
+                else {
+                    filtered = data.filter(a => a.district == "#selectDistrictDD".value)
+                }
+
+
+            var temp = "";
+            if (filtered.length > 0) {
+                var temp = "";
+                filtered.forEach((itemData) => {               
+                    temp += "<tr>";
+                    temp += "<td>" + itemData.disclosure_year + "</td>";
+                    temp += "<td>" + itemData.transaction_date + "</td>";
+                    temp += "<td>" + itemData.district + "</td>";
+                    temp += "<td>" + itemData.representative + "</td>";
+                    temp += "<td>" + itemData.ticker + "</td>";
+                    temp += "<td>" + itemData.amount + "</td>";
+                    temp += "<td>" + itemData.asset_description + "</td></tr>";
+                });
+                document.getElementById('#data').innerHTML = temp;
 
 
                 
@@ -126,22 +183,6 @@ function getData() {
 
 
 
-//new stuff from filter 
-    // var json = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
-    // var js = JSON.parse(json);
-    // function getKeys(obj, val) {
-    //     var objects = [];
-    //     for (var i in obj) {
-    //         if (!obj.hasOwnProperty(i)) continue;
-    //         if (typeof obj[i] == 'object') {
-    //             objects = objects.concat(getKeys(obj[i], val));
-    //         } else if (obj[i] == val) {
-    //             objects.push(i);
-    //         }
-    //     }
-        
-    // }
-    // console.log(getKeys(js, "district"))
 
 }        
 
